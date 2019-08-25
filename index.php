a37432548e7827fabaa9ea5caad0fd2cd342a1a0
chrischaplow <47055792+chrischaplow@users.noreply.github.com>
2019-08-25 13:23:41 +0200

    File uploaded

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+<?php 
+$a=100; $b=200;
+$result1=($a<$b) ? "apple <br>" : "banana <br>";
+echo $result1;
+$result2=($a>$b) ? "apple <br>" : "banana <br>";
+echo $result2;
+?> 
+        
+    </body>
+</html>
